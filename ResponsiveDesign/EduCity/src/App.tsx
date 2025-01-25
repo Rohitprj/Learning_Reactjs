abf9a2ae238b3d7c2fd064ea4eddd5dd637bb565
Rohit kumar <rohitprj.1282@gmail.com>
2025-01-25 18:31:00 +0530

    Add Programes component with styling to display program details

--- ResponsiveDesign/EduCity/src/App.tsx
+++ ResponsiveDesign/EduCity/src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Navbar } from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Programes from "./components/programes/Programes";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Programes />
+      </div>
     </div>
   );
 };
--- ResponsiveDesign/EduCity/src/components/programes/Programes.tsx
+++ ResponsiveDesign/EduCity/src/components/programes/Programes.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import "./Programes.css";
+import graduation3 from "../../assets/graduation3.jpg";
+import grad from "../../assets/graduation-cap-svgrepo-com.svg";
 
 const Programes = () => {
   return (
-    <div>
-      <h1>Our Programes</h1>
+    <div className="programes">
+      <div className="programe">
+        <img src={graduation3} alt="" />
+        <div className="caption">
+          <img src={grad} alt="" />
+          <p>Graduation Degree</p>
+        </div>
+      </div>
+
+      <div className="programe">
+        <img src={graduation3} alt="" />
+        <div className="caption">
+          <img src={grad} alt="" />
+          <p>Graduation Degree</p>
+        </div>
+      </div>
+
+      <div className="programe">
+        <img src={graduation3} alt="" />
+        <div className="caption">
+          <img src={grad} alt="" />
+          <p>Graduation Degree</p>
+        </div>
+      </div>
     </div>
   );
 };
